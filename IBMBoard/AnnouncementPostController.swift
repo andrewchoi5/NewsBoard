5a5c40b3f22f87bc893c48f7d009ace7fafe75d7
Andrew Frolkin <frolkina@ca.ibm.com>
2016-04-13 11:34:56 -0400

    New post button style.

--- IBMBoard/AnnouncementPostController.swift
+++ IBMBoard/AnnouncementPostController.swift
@@ -14,16 +14,19 @@ class AnnouncementPostController : PosterController, UIImagePickerControllerDele
     @IBOutlet weak var announcementText: UITextView!
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var announcementPhotoURL: UITextField!
+    @IBOutlet weak var announcementPostButton: UIBarButtonItem!
     
     var selectedImage : UIImage?
     
+    let greenTint = UIColor(red: 92/255.0, green: 255.0/255.0, blue: 111/255.0, alpha: 255.0/255.0)
+    let greyTint = UIColor(red: 63/255.0, green: 69.0/255.0, blue: 77/255.0, alpha: 255.0/255.0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        announcementPostButton.tintColor = greyTint
     }
     
-    @IBAction func didPushPostButton(sender: UIButton) {
+    @IBAction func didPushPostButton(sender: UIBarButtonItem) {
         
         if !progressBar.hidden {
             return
