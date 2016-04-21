41ca49b6aca958219546346082554816180d458c
Andrew Frolkin <frolkina@ca.ibm.com>
2016-04-21 17:15:23 -0400

    Clean up cross drawing

--- IBMBoard/DefaultCellView.swift
+++ IBMBoard/DefaultCellView.swift
@@ -24,51 +24,45 @@ class DefaultCellView : UICollectionViewCell {
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        // add cross
-        line.translatesAutoresizingMaskIntoConstraints = false
-        line.backgroundColor = UIColor.darkTextColor().colorWithAlphaComponent(0.05)
-        
-        clipsToBounds = true // Cut off everything outside the view
-        
-        addSubview(line)
-        
-        // Define line width
-        line.addConstraint(NSLayoutConstraint(item: line, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 1))
-        
-        // Set up line length constraint
-        let lengthConstraint = NSLayoutConstraint(item: line, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 0)
-        addConstraint(lengthConstraint)
-        
-        // Center line in view
-        addConstraint(NSLayoutConstraint(item: line, attribute: .CenterX, relatedBy: .Equal, toItem: self, attribute: .CenterX, multiplier: 1, constant: 0))
-        addConstraint(NSLayoutConstraint(item: line, attribute: .CenterY, relatedBy: .Equal, toItem: self, attribute: .CenterY, multiplier: 1, constant: 0))
-        lengthConstraint.constant = sqrt(pow(frame.size.width, 2) + pow(frame.size.height, 2))
-        line.transform = CGAffineTransformMakeRotation(atan2(frame.size.height, frame.size.width))
-        
-        
-        line2.translatesAutoresizingMaskIntoConstraints = false
-        line2.backgroundColor = UIColor.darkTextColor().colorWithAlphaComponent(0.05)
-        
-        addSubview(line2)
-        
-        // Define line width
-        line2.addConstraint(NSLayoutConstraint(item: line2, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 1))
-        
-        // Set up line length constraint
-        let lengthConstraint2 = NSLayoutConstraint(item: line2, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 0)
-        addConstraint(lengthConstraint2)
-        
-        // Center line in view
-        addConstraint(NSLayoutConstraint(item: line2, attribute: .CenterX, relatedBy: .Equal, toItem: self, attribute: .CenterX, multiplier: 1, constant: 0))
-        addConstraint(NSLayoutConstraint(item: line2, attribute: .CenterY, relatedBy: .Equal, toItem: self, attribute: .CenterY, multiplier: 1, constant: 0))
-        lengthConstraint2.constant = sqrt(pow(frame.size.width, 2) + pow(frame.size.height, 2))
-        line2.transform = CGAffineTransformMakeRotation(CGFloat(M_PI) - atan2(frame.size.height, frame.size.width))
-    
+        clipsToBounds = true
+        self.drawCross()
     }
     
     func hideCross() {
         line2.hidden = true;
         line.hidden = true;
+    }
+    
+    private func drawCross() {
+        line.translatesAutoresizingMaskIntoConstraints = false
+        line.backgroundColor = UIColor.darkTextColor().colorWithAlphaComponent(0.05)
+        line2.translatesAutoresizingMaskIntoConstraints = false
+        line2.backgroundColor = UIColor.darkTextColor().colorWithAlphaComponent(0.05)
+        
+        addSubview(line)
+        addSubview(line2)
+
+        // Define line widths
+        line.addConstraint(NSLayoutConstraint(item: line, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 1))
+        line2.addConstraint(NSLayoutConstraint(item: line2, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 1))
+        
+        // Set up line length constraints
+        let lengthConstraint = NSLayoutConstraint(item: line, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 0)
+        addConstraint(lengthConstraint)
+        
+        let lengthConstraint2 = NSLayoutConstraint(item: line2, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 0)
+        addConstraint(lengthConstraint2)
+        
+        // Center lines in view
+        addConstraint(NSLayoutConstraint(item: line, attribute: .CenterX, relatedBy: .Equal, toItem: self, attribute: .CenterX, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: line, attribute: .CenterY, relatedBy: .Equal, toItem: self, attribute: .CenterY, multiplier: 1, constant: 0))
+        lengthConstraint.constant = sqrt(pow(frame.size.width, 2) + pow(frame.size.height, 2))
+        line.transform = CGAffineTransformMakeRotation(atan2(frame.size.height, frame.size.width))
+        
+        addConstraint(NSLayoutConstraint(item: line2, attribute: .CenterX, relatedBy: .Equal, toItem: self, attribute: .CenterX, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: line2, attribute: .CenterY, relatedBy: .Equal, toItem: self, attribute: .CenterY, multiplier: 1, constant: 0))
+        lengthConstraint2.constant = sqrt(pow(frame.size.width, 2) + pow(frame.size.height, 2))
+        line2.transform = CGAffineTransformMakeRotation(CGFloat(M_PI) - atan2(frame.size.height, frame.size.width))
     }
     
 }
